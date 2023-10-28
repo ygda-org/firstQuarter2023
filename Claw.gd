446e58cf17cd89acf9f759729953d308d204b4be
simplyorange8 <thisiskyle8@gmail.com>
2023-10-27 22:24:18 -0400

    I made the claw all directions
    I also deleted bugfix code, fixed the animations, and fixed the collider

--- Claw.gd
+++ Claw.gd
@@ -8,20 +8,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("ready")
 	$Despawn.start()
 
 #func _set_claw_direction
 func _process(delta):
-	print("physics")
+	pass
 
 
 func _on_Despawn_timeout():
-	print("hiiii")
 	queue_free()
 
 
 func _on_ClawHitBox_body_entered(body):
-	print("byeee")
 	if "EnemyTest" in body.name:
 		body._dead()
--- Player/Player.gd
+++ Player/Player.gd
@@ -23,13 +23,28 @@ func _physics_process(delta):
 		direction = 0
 	else:
 		velocity.y = 0	
+	if dirList[direction] == "Right" or dirList[direction] == "Left":
+		$CollisionPolygon2D.rotation_degrees = 90
+	else:
+		$CollisionPolygon2D.rotation_degrees = 0
 	if(velocity.x != 0 or velocity.y != 0):	
 		$AnimationPlayer.play("walk" + dirList[direction])
 	else:
 		$AnimationPlayer.play("idle" + dirList[direction])
 	if Input.is_action_just_pressed("ui_select"):
 		var claw = CLAW.instance()
-		claw.position = $Claw_Position.global_position
+		if dirList[direction] == "Right":
+			claw.position = $Claw_PositionRight.global_position
+			claw.rotation_degrees = 0
+		elif dirList[direction] == "Left":
+			claw.position = $Claw_PositionLeft.global_position
+			claw.rotation_degrees = 0
+		elif dirList[direction] == "Up":
+			claw.position = $Claw_PositionUp.global_position
+			claw.rotation_degrees = 90
+		else:
+			claw.position = $Claw_PositionDown.global_position
+			claw.rotation_degrees = 90
 		#claw._set_claw_direction(direction)
 		get_parent().add_child(claw)
 	move_and_slide(velocity * SPEED)
