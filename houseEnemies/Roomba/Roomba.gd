153f23f4123daf8d9200939cc87ea034171b7e4f
Penguinja8 <144949726+Penguinja8@users.noreply.github.com>
2023-11-01 18:33:17 -0400

    Roomba, Dash, and organization
    made a roomba enemy that is basically just a screensaver
    
    made a dash for the player, currently activates when pressing tab (couldn't think of anything else lol)
    
    did some organization - added comments to the player code to seperate different parts and made folders to store files

---
+++ houseEnemies/Roomba/Roomba.gd
@@ -0,0 +1,50 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var velocity = Vector2()
+var speed = 50
+var tempVelocity = Vector2()
+var stopped = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	velocity = Vector2(speed,speed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass\
+func _physics_process(delta):
+	move_and_slide(velocity)
+	
+
+
+
+func _on_Bounce_hit_box_body_entered(body):
+	if stopped == false:
+		stopped = true
+		tempVelocity = velocity
+		velocity = Vector2(0,0)
+		$RoombaPause.start()
+		
+
+
+func _on_RoombaPause_timeout():
+	stopped = false
+	if tempVelocity.x > 0:
+		if tempVelocity.y > 0:
+			velocity = Vector2(speed,-1*speed)
+		else:
+			velocity = Vector2(-1*speed,-1*speed)
+	else:
+		if tempVelocity.y > 0:
+			velocity = Vector2(speed,speed)
+		else:
+			velocity = Vector2(-1*speed,speed)
+
+func _dead():
+	queue_free()
+	
